4542568e63e92d98487ba0cb60f0ee0714817bef
Jeff Hacker <reachjkh@yahoo.com>
2015-05-27 11:32:25 -0400

    initial

---
+++ normal_square_root.py
@@ -0,0 +1,10 @@
+# Normal Mode
+p  = int(input("What positive number do you want to find the Square Root? > "))
+g = int(input("Guess what you think the Square Root may be. > "))
+count = 0
+
+while count <= 8:
+    x = p / g
+    g = (x + g) / 2
+    count += 1
+print("The Square Root of {} is {}.".format(p, g))
